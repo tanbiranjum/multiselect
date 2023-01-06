bd19264706db4a9e01a653daaecdd287d50cdc51
tanbiranjum <tanbiranjum@hotmail.com>
2023-01-06 20:11:27 +0600

    all basic structure done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,90 @@
 import MultiSelect from "./components/MultiSelect";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [selected, setSelected] = useState([]);
+  const [terms, setTerms] = useState(false);
+
+  const handleChange = (e) => {
+    const options = e.target.options;
+    for (let i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        options[i].style.backgroundColor = "#D0C1FC";
+        if (selected.length > 0) {
+          setSelected([
+            ...selected,
+            { value: options[i].value, index: i, name: options[i].label },
+          ]);
+        } else {
+          setSelected([
+            { value: options[i].value, index: i, name: options[i].label },
+          ]);
+        }
+      }
+    }
+  };
+
+  const onSubmit = () => {
+    console.log(name, selected, terms);
+  };
+
   return (
-    <div className="App">
-      <MultiSelect />
+    <div className="App p-24 bg-slate-500 min-h-screen">
+      <h1 className="text-2xl text-gray-50 mb-3">
+        Please enter your name and pick the Sectors you are currently involved
+        in.
+      </h1>
+      <hr className="w-3/4" />
+      <div className="mt-4">
+        <div className="space-y-1 text-sm">
+          <label for="name" className="block dark:text-gray-50 text-lg">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            className="w-1/5 px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:dark:border-violet-400"
+          />
+        </div>
+        <div className="space-y-1 text-sm mt-4">
+          <label for="select" className="block dark:text-gray-50 text-lg">
+            Sectors
+          </label>
+          <MultiSelect
+            handleChange={handleChange}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        </div>
+        <div className="flex items-center mt-3">
+          <input
+            type="checkbox"
+            name="remember"
+            id="remember"
+            aria-label="Remember me"
+            onChange={(e) => {
+              setTerms(e.target.checked);
+            }}
+            className="mr-1 rounded-sm focus:ring-violet-400 focus:dark:border-violet-400 focus:ring-2 accent-violet-400"
+          />
+          <label for="remember" className="text-md dark:text-gray-50">
+            Agree to terms
+          </label>
+        </div>
+        <button
+          type="button"
+          onClick={onSubmit}
+          className="px-8 mt-4 py-2 font-semibold rounded bg-violet-400 text-gray-50"
+        >
+          save
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,59 +1,72 @@
-import Select from "react-select";
+import React, { useState, useRef } from "react";
+import Data from "../data/select.json";
 
-const options = [
-  {
-    label: "Manufacturing",
-    value: "chocolate",
-    options: [
-      {
-        label: "Construction materials",
-        value: "19",
-      },
-      {
-        label: "Electronics and Optics",
-        value: "18",
-      },
-      
-    ],
-  },
-  {
-    label: "Food and Beverage",
-    value: "chips",
-    options: [
-      {
-        label: "Backery & Confectionery Products",
-        value: "6",
-      },
-      {
-        label: "Beverages",
-        value: "6",
-      },
-      {
-        label: "Fish & Fish Products",
-        value: "6",
-      },
-      {
-        label: "Meat & Meat Products",
-        value: "6",
-      },
-      {
-        label: "Milks & Dairy Products",
-        value: "6",
-      },
-      {
-        label: "Sweets & Snack Food",
-        value: "6",
-      },
-      {
-        label: "Other",
-        value: "6",
-      },
-    ],
-  },
-];
+const MultiSelect = ({ handleChange, selected, setSelected }) => {
+  const deleteItem = (index) => {
+    const options = ref.current.options;
+    options[index].style.backgroundColor = "transparent";
+    options[index].selected = false;
+    options[index].style.color = "white";
+    const newSelected = selected.filter((item) => item.index !== index);
+    setSelected([...newSelected]);
+  };
 
-const MultiSelect = () => {
-  return <Select name="Multi Select" options={options} isMulti />;
+  const ref = useRef();
+  return (
+    <div className="mt-2">
+      <select
+        multiple
+        size="5"
+        onChange={handleChange}
+        ref={ref}
+        className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 p-2.5 bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        {Data.map((item, index) => (
+          <>
+            <option key={index} value={item.value} className="text-lg">
+              {item.label}
+            </option>
+            {item.options.map((option, index) => (
+              <>
+                <option key={index} value={option.value} className="text-lg">
+                  &nbsp;&nbsp;&nbsp;&nbsp;{option.label}
+                </option>
+                {option.options &&
+                  option.options.map((option, index) => (
+                    <option
+                      key={index}
+                      value={option.value}
+                      className="text-lg"
+                    >
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      {option.label}
+                    </option>
+                  ))}
+              </>
+            ))}
+          </>
+        ))}
+      </select>
+      <div className="flex mt-3 gap-x-2">
+        {selected?.map((item, index) => (
+          <div
+            className="bg-violet-400 text-white p-2 flex gap-x-2 rounded-sm"
+            key={index}
+          >
+            {item.name}
+            <button
+              className="text-gray-700 font-bold"
+              onClick={() => {
+                deleteItem(item.index);
+              }}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MultiSelect;
